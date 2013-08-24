4b3ac6f8c24f550b23dc62c3b30d538678efbaf0
Harald Kraemer <harryk@freenet.de>
2013-08-24 09:29:42 +0200

    added reactor drawing

--- src/game.js
+++ src/game.js
@@ -2,6 +2,7 @@
     "use strict";
     var empty = " ";
     var wall = "wall";
+    var core = "core";
     var level = [
         [wall, wall , wall , wall , wall , wall , wall , wall , wall],
         [wall, empty, empty, empty, empty, empty, empty, empty, wall],   
@@ -15,6 +16,9 @@
         x : 1
     };
 
+    var core = {
+        x : 5
+    }
     var keysDown = {};
 
     function draw_level() {
@@ -52,6 +56,39 @@
         context.fill();
     }
 
+    function draw_reactor() {
+        var canvas = document.getElementById('game_screen');
+        var y = 4 * 50;
+        var x = core.x * 50;
+
+        var context = canvas.getContext('2d');
+        context.fillStyle = "rgb(127, 127, 127)";
+        context.beginPath();
+        context.moveTo(x, y);
+        context.lineTo(x+50, y);
+        context.lineTo(x+40, y+10);
+        context.lineTo(x+10, y+10);
+        context.lineTo(x, y);
+        context.fill();
+
+        context.beginPath();
+        context.moveTo(x, y+50);
+        context.lineTo(x+50, y+50);
+        context.lineTo(x+40, y+40);
+        context.lineTo(x+10, y+40);
+        context.lineTo(x, y+50);
+        context.fill();
+
+        context.fillStyle = "rgb(0, 0, 255)";
+        context.beginPath();
+        context.moveTo(x+25, y+10);
+        context.lineTo(x+10, y+25);
+        context.lineTo(x+25, y+40);
+        context.lineTo(x+40, y+25);
+        context.lineTo(x+25, y+10);
+        context.fill();
+    }
+
     scope.addEventListener("keydown", function(e) {
         keysDown[e.keyCode] = true;
     }, false);
@@ -81,6 +118,7 @@
         }
         draw_level();
         draw_crane();
+        draw_reactor();
     }
 
     window.setInterval(update, 1);
